c8205c3c86d9df9fc628cb49ebe8ac0bb03512bd
Matty <matty@mattycollins.com.au>
2016-03-08 22:37:18 +1100

    add - API pkmn name endpoint tests

--- test/services/pkmnInfoSpec.js
+++ test/services/pkmnInfoSpec.js
@@ -1,5 +1,5 @@
 // scaffold for angulars
-var pkmnInfo;
+var infoServices;
 var $httpBackend;
 
 describe('service: Pkmn Info', function() {
@@ -8,11 +8,10 @@ describe('service: Pkmn Info', function() {
   beforeEach(inject(function(_infoServices_, _$httpBackend_) {
     $httpBackend = _$httpBackend_;
 
-    // $httpBackend.when('GET', mockPkmn.endPoint.byName).respond(mockPkmn.results.name);
-    // $httpBackend.when('GET', mockPkmn.endPoint.byId).respond(mockPkmn.results.id);
-    // $httpBackend.when('GET', mockPkmn.endPoint.fail).respond(mockPkmn.results.fail);
+    $httpBackend.when('GET', mockPkmn.endPoint.byId).respond(mockPkmn.results.id);
+    $httpBackend.when('GET', mockPkmn.endPoint.byName).respond(mockPkmn.results.name);
 
-    pkmnInfo = _infoServices_;
+    infoServices = _infoServices_;
   }));
 
   afterEach(function(){
@@ -20,7 +19,13 @@ describe('service: Pkmn Info', function() {
     $httpBackend.verifyNoOutstandingRequest();
   });
 
-  it('should get pkmn by name');
+  it('should hit individual pkmn by name/id endpoint', function() {
+    infoServices.getPkmnByName('gastly');
+    expect($httpBackend.flush).not.toThrow();
+
+    infoServices.getPkmnByName('25');
+    expect($httpBackend.flush).not.toThrow();
+  });
 
   it('should get pkmn by id');
 });
